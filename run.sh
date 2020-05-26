5d2815fba49da03f5b221ec3ccece67e3418d31b
Alex Burkey <awburkey@gmail.com>
2020-05-26 20:18:03 +0100

    Add wrapper script as a way to easily keep the bot running if it unexpectedly crashes.

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FAILS=0
+
+while true
+do
+  sleep 0.5
+  python3 -u bot.py # your program
+  EXIT=$?
+  ((FAILS++))
+
+  if [[ $FAILS -gt 50 ]]
+  then
+    echo "[$(date)] failed to many times. aborting ..."
+    exit 1
+  fi
+
+  echo "[$(date)] bot exited with code $EXIT. restarting ..."
+
+done
