ecdda28cf3f84c2f98749ccdb5debd7422de92e6
Alex Burkey <awburkey@gmail.com>
2020-07-08 08:55:38 +0100

    Redirect output of python file to bot logs, run.sh to monitor logs and is-running.sh to cron logs

--- is-running.sh
+++ is-running.sh
@@ -2,14 +2,20 @@
 set -x #echo on
 
 # print env
-pwd
 echo "~~~~~~"
+pwd
 date
 
-if [[ $(ps -e | grep run.sh) ]]; then
+
+# TODO: Might also want to check if there are any python3 
+# processes running.
+# We could get multiple instances of bot processes running
+if [[ $(pgrep run.sh) ]]; then
     echo "Bot is running"
 else
     echo "Bot is NOT running"
     echo "Restarting bot..."
-    nohup ./run.sh &
+    # Sends output of run.sh to ./logs/monitor
+    # info about when and how many times the bot has crashed
+    #nohup ./run.sh >> "./logs/monitor/$(date +"%FT%H%M%S").out" &
 fi
