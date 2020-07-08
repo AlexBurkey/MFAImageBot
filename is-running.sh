7f1c42f677925ce95b2b383b9a8d9a87fafad833
Alex Burkey <awburkey@gmail.com>
2020-07-08 07:48:29 +0100

    Add cron job monitor script.
    Script is run every 10 minutes and prints a separator line and the date
    then checks if the bot is currently running and runs the nohup command if not.
    
    NOTE: This script BREAKS if the shell script to run the bot (run.sh) is changed.

---
+++ is-running.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x #echo on
+
+# print env
+pwd
+echo "~~~~~~"
+date %c
+
+if [[ $(ps -e | grep run.sh) ]]; then
+    echo "Bot is running"
+else
+    echo "Bot is NOT running"
+    echo "Restarting bot..."
+    nohup ./run.sh &
+fi
