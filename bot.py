20c901fcedcfb1a529ab1cea1e7b36d8e47deb6c
Alex Burkey <awburkey@gmail.com>
2020-07-10 09:17:43 +0100

    Attempting to remove need for commands and major refactor moving off developing on the RPi to test

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ import requests
 from string import Template
 from dotenv import load_dotenv
 import my_strings as ms
+import helpers as h
 
 
 # References praw-ini file
@@ -22,13 +23,71 @@ def run():
     load_dotenv()  # Used for imgur auth
     # TODO: verify that the db path is valid.
     #   A single file is fine but dirs are not created if they don't exist
-    db_setup(DB_FILE)  # TODO: set db file path as CLI parameter
     print("Looking for comments...")
     for comment in r.subreddit(SUBREDDIT_NAME).stream.comments():
         if check_batsignal(comment.body) and not check_has_responded(comment):
-            print(f"Comment hash: {comment}")
-            # TODO: Set respond bool as CLI input value
-            bot_action(comment, respond=RESPOND)
+            response = ms.HELP_TEXT
+            tokens = h.get_and_split_first_line(comment.body)
+            # More than 100 tokens on the first line. 
+            # I'm not dealing with that
+            if len(tokens) > 100:
+                db_obj = h.reply_and_upvote(comment, response=ms.HELP_TEXT, respond=RESPOND)
+                add_comment_to_db(db_obj)
+                continue
+
+            # Check for help
+            if parse_comment(tokens)['help']:
+                db_obj = h.reply_and_upvote(comment, response=ms.HELP_TEXT, respond=RESPOND)
+                add_comment_to_db(db_obj)
+                continue
+
+            index = parse_comment(tokens)['index']
+            # TODO: Wrap/deal with possible exceptions from parsing imgur url
+            comment_imgur_url = parse_comment(tokens)['imgur_url']
+            
+            # Check/parse imgur
+            album_link_map = None
+            album_link = None
+            if comment_imgur_url is not None:
+                album_link_map = parse_imgur_url(comment_imgur_url)
+                album_link = comment_imgur_url
+            elif h.is_imgur_url(comment.submission.url):
+                album_link_map = parse_imgur_url(comment.submission.url)
+                album_link = comment.submission.url
+            else:
+                # No imgur link found. Respond
+                response = 'Sorry, no imgur link found in your comment or the link of the OP.'
+                db_obj = h.reply_and_upvote(comment, response=response, respond=RESPOND)
+                add_comment_to_db(db_obj)
+                continue
+            
+            album_link_type = album_link_map['type']
+            album_link_id   = album_link_map['id']
+            
+            # TODO: Wrap in try/except
+            request_url = build_request_url(album_link_type, album_link_id)
+            
+            r = send_imgur_api_request(request_url)
+            if r is not None and r.status_code == 200:
+                response = None
+                try:
+                    # Parse request and set response text
+                    image_link = get_direct_image_link(r.json(), album_link_type, index)
+                    print(f'Image link: {image_link}')
+                    s = Template(DIRECT_LINK_TEMPLATE)
+                    response = s.substitute(index=index, image_link=image_link, album_link=album_link)
+                except IndexError as e:
+                    response = 'Sorry that index is out of bounds.'
+                db_obj = h.reply_and_upvote(comment, response=response, respond=RESPOND)
+                add_comment_to_db(db_obj)
+                continue 
+            else:  # Status code not 200
+                # TODO: Deal with status codes differently, like if imgur is down or I don't have the env configured
+                print(f'Status Code: {r.status_code}')
+                response = f'Sorry, {album_link} is probably not an existing imgur album, or Imgur is down.'
+                db_obj = h.reply_and_upvote(comment, response=response, respond=RESPOND)
+                add_comment_to_db(db_obj)
+                continue
 
 
 def check_batsignal(comment_body):
@@ -71,9 +130,56 @@ def check_has_responded(comment):
     return val
 
 
+def parse_comment(tokens):
+    pairs = {'index': -1, 'imgur_url': None, 'help': False}
+    # Find the numbers in the tokens
+    for s in tokens:
+        # Look for help
+        if s.lower() == 'help':
+            pairs['help'] = True
+            break
+        # Find the numbers in the tokens
+        if h.isInt(s):
+            pairs['index'] = int(s)
+        # Find imgur URL in the tokens
+        if h.is_imgur_url(s):
+            pairs['imgur_url'] = s
+    return pairs
+
+
+def build_request_url(imgur_link_type, imgur_link_id):
+    """
+    Create the Imgur Request URL.
+    Imgur Galleries and Albums have different endpoints.
+    Any other resource type hasn't been implemented/might not exist.
+    TODO: doctests
+    """
+    if imgur_link_type == 'album':
+        s = Template(IMGUR_ALBUM_API_URL)
+        return s.substitute(album_hash=imgur_link_id)
+    elif imgur_link_type == 'gallery':
+        s = Template(IMGUR_GALLERY_API_URL)
+        return s.substitute(gallery_hash=imgur_link_id)
+    else:
+        raise ValueError('Sorry, that imgur resource hasn\'t been implemented yet.')
+
+
+def send_imgur_api_request(request_url):
+    """
+    Send API request to Imgur
+    TODO: doctests
+    """
+    # Send the request
+    client_id = os.getenv('IMGUR_CLIENT_ID')
+    headers = {'Authorization': f'Client-ID {client_id}'}
+    print(f"Request url: {request_url}")
+    return requests.get(request_url, headers=headers)
+
+
 def bot_action(c, verbose=True, respond=False):
     response_text = 'bot_action text'
     response_type = None
+    # TODO: Get the first line only
     tokens = c.body.split()
     # If there's a command in the comment parse and react
     # Break this out into a "parse_comment_tokens" function
@@ -127,72 +233,23 @@ def bot_action(c, verbose=True, respond=False):
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
-def get_direct_image_link(comment, tokens):
+
+def get_direct_image_link(r_json, imgur_link_type, index):
     """
-    Gets the direct link to an image from an imgur album based on index.
-    Tokens should look like:
-    ['!MFAImageBot', 'link', '<imgur-link>', '<index>']
-    or
-    ['!MFAImageBot', 'op', '<index>']
+    Parse Imgur API request response JSON for the direct image link.
+    If the index is out of bounds will throw an IndexError.
+
+    TODO: Tests for this are sorely needed. Unsure what happens with all of this refactoring.
+    I _think_ only an IndexError is thrown now.
     """
-    imgur_url = None
-    index = None
-    image_link = ''
-    if len(tokens) >= 4 and tokens[1] == 'link':
-        # Correct num parameters for album provided
-        imgur_url = tokens[2]
-        index = get_index_from_string(tokens[3])
-    elif len(tokens) >= 3 and tokens[1] == 'op':
-        # Correct num parametrs for album in OP
-        # This should generate a 404 or something if the post isn't a link to imgur.
-        imgur_url = comment.submission.url
-        index = get_index_from_string(tokens[2])
+    if imgur_link_type == 'gallery':
+        # Gallery: g_response.data.images[index].link
+        return r_json['data']['images'][index-1]['link']
+    elif imgur_link_type == 'album':
+        # Album: a_response.data[index].link
+        return r_json['data'][index-1]['link']
     else:
-        print('Looks like a malformed `link` or `op` command')
-        raise ValueError(f'Malformed `{tokens[1]}`` command.')
-
-    # This can raise an exception which is fine
-    link_type_and_id = parse_imgur_url(imgur_url)
-    imgur_resource_type = link_type_and_id['type']
-    resource_id = link_type_and_id['id']
-    r = None
-    if link_type_and_id is not None:
-        # Galleries and Albums have different URL endpoints
-        url = ''
-        if imgur_resource_type == 'album':
-            s = Template(IMGUR_ALBUM_API_URL)
-            url = s.substitute(album_hash=resource_id)
-        elif imgur_resource_type == 'gallery':
-            s = Template(IMGUR_GALLERY_API_URL)
-            url = s.substitute(gallery_hash=resource_id)
-        else:
-            raise ValueError('Sorry, that imgur resource hasn\'t been implemented yet.')
-
-        # Send the request
-        client_id = os.getenv('IMGUR_CLIENT_ID')
-        headers = {'Authorization': f'Client-ID {client_id}'}
-        print(f"Request url: {url}")
-        r = requests.get(url, headers=headers)
-
-    # Since I thought 2 lines would make sense here this is probably a good place to separate methods
-    if r is not None and r.status_code == 200:
-        # happy path for now
-        r_json = r.json()
-        image_link = ''
-        if imgur_resource_type == 'gallery':
-            # Gallery: g_response.data.images[index].link
-            image_link = r_json['data']['images'][index-1]['link']
-        elif imgur_resource_type == 'album':
-            # Album: a_response.data[index].link
-            image_link = r_json['data'][index-1]['link']
-        else:
-            raise ValueError('This should be unreachable. Please respond to this comment or open an issue so I see it.')
-        print(f'Image link: {image_link}')
-        return {'image_link': image_link, 'index': index, 'album_link': imgur_url}
-    else:  # Status code not 200
-        # TODO: Deal with status codes differently, like if imgur is down or I don't have the env configured
-        print(f'Status Code: {r.status_code}')
-        raise ValueError(f'Sorry, {imgur_url} is probably not an existing imgur album.')
+        raise ValueError('This should be unreachable. Please respond to this comment or open an issue so I see it.')   
 
 
 def get_index_from_string(str):
@@ -260,11 +317,45 @@ def parse_imgur_url(url):
     }
 
 
+def is_number_list(string):
+    """
+    Trying to build a method to check for lists for numbers
+
+    >>> is_number_list('1,2,3')
+    True
+    >>> parse_imgur_url('HtTP://imgur.COM:80/gallery/59npG')
+    {'id': '59npG', 'type': 'gallery'}
+    >>> parse_imgur_url('https://i.imgur.com/altd8Ld.png')
+    {'id': 'altd8Ld', 'type': 'image'}
+    >>> parse_imgur_url('https://i.stack.imgur.com/ELmEk.png')
+    {'id': 'ELmEk', 'type': 'image'}
+    >>> parse_imgur_url('http://not-imgur.com/altd8Ld.png') is None
+    Traceback (most recent call last):
+      ...
+    ValueError: Sorry, "http://not-imgur.com/altd8Ld.png" is not a valid imgur URL
+    >>> parse_imgur_url('tftp://imgur.com/gallery/59npG') is None
+    Traceback (most recent call last):
+      ...
+    ValueError: Sorry, "tftp://imgur.com/gallery/59npG" is not a valid imgur URL
+    >>> parse_imgur_url('Blah') is None
+    Traceback (most recent call last):
+      ...
+    ValueError: Sorry, "Blah" is not a valid imgur URL
+    """
+    match = re.match(r'^[1-9]+(,[1-9]+)*$', string)
+
 def add_comment_to_db(db_dict):
+    """
+    Adds the comment and its info to the database
+    """
+    # print(f"Hash: {db_dict['hash']}")
+    print(f"Has responded: {db_dict['has_responded']}")
+    print(f"Response text: ")
+    print(f"{db_dict['response_text']}")
     conn = sqlite3.connect(DB_FILE)
     cur = conn.cursor()
     # https://stackoverflow.com/questions/19337029/insert-if-not-exists-statement-in-sqlite
-    cur.execute('INSERT OR REPLACE INTO comments VALUES (:hash, :has_responded, :response_type)', db_dict)
+    cur.execute('INSERT OR REPLACE INTO comments VALUES (:hash, :has_responded, :response_text)', db_dict)
     conn.commit()
     conn.close()
 
@@ -276,7 +367,7 @@ def db_setup(db_file):
     cur.execute('''CREATE TABLE IF NOT EXISTS comments (
         comment_hash  TEXT       NOT NULL  UNIQUE,
         has_responded INTEGER    DEFAULT 0,
-        response_type TEXT       DEFAULT NULL,
+        response_text TEXT       DEFAULT NULL,
         CHECK(has_responded = 0 OR has_responded = 1)
     )''')
     conn.commit()
@@ -311,4 +402,6 @@ if __name__ == '__main__':
     print(f"DB file: {DB_FILE}")
     print(f"Subreddit name: {SUBREDDIT_NAME}")
     print(f"Respond: {RESPOND}")
+    print("~~~~~~~~~~")
+    db_setup(DB_FILE)  # TODO: set db file path as CLI parameter
     run()
