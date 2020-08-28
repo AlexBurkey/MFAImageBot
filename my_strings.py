560130abfe91cadce0d2831b0fc4fe65be7c924e
Hussain Jaffery <hussainjaffery@users.noreply.github.com>
2020-08-28 00:29:44 -0400

    Fix linting in codebase and GH Action (#30)
    * Rename my_strings to strings
    
    * Rename my_strings to constants
    
    * Add GH token to linter action
    
    * Reformat constants.py for black python linter
    
    * Fixing some linting stuff
    
    * Fix more linting stuff
    
    * Fix doctest quotes
    
    * Fix more linter errors.
    Note: GH super linter doesn't work well with pylint import-errors since it's hard to install dependencies in the right place. Ignoring them since they don't matter for the linting action anyway.
    
    * Fix hopefully final lint issue
    
    * Revert "Rename my_strings to strings"
    
    This reverts commit 0e761c2e0e1a782031bd2f3c7c4f4ff7fbd480e9.
    
    * Revert "Rename my_strings to constants"
    
    This reverts commit 1a3a957dffe6540aa265f48ec9369d073e68c521.
    
    * Fix formatting in reverted my_strings file
    
    * Fix linting and accidental double quote
    
    Co-authored-by: Alex Burkey <alex.w.burkey@gmail.com>

--- my_strings.py
+++ my_strings.py
@@ -1,18 +1,20 @@
-USER_AGENT = 'MFAImageBot'
+USER_AGENT = "MFAImageBot"
 
-DB_FILE = 'mfa.db'
+DB_FILE = "mfa.db"
 SUBREDDIT_NAME = "malefashionadvice"
 
-
 TODO_TEXT = "Sorry, this function has not been implemented yet.\n\n"
-HELP_TEXT = ("Usage: I respond to comments starting with `!MFAImageBot` (case insensitive).  \n"
-             "`!MFAImageBot help`: Print this help message.  \n"
-             "`!MFAImageBot <album-link> 2, 3, 42`: I attempt to directly link the 2nd, 3rd, and 42nd images from <album-link>  \n"
-             "`!MFAImageBot 2, 3, 42`: I attempt to directly link the 2nd, 3rd, and 42nd images from the album in the submission  \n"
-             "Comma or whitespace separated lists of numbers are also cool: `!MFAImageBot 2,4,42` or `!MFAImageBot 2 4 42`  \n"
-             )
-TAIL = ("\n\n---\nI am a bot! If you've found a bug you can open an issue "
-        "[here.](https://github.com/AlexBurkey/MFAImageBot/issues/new?template=bug_report.md)  \n"
-        "If you have an idea for a feature, you can submit the idea "
-        "[here](https://github.com/AlexBurkey/MFAImageBot/issues/new?template=feature_request.md)  \n"
-        "Version 1.2")
+HELP_TEXT = (
+    "Usage: I respond to comments starting with `!MFAImageBot` (case insensitive).  \n"
+    "`!MFAImageBot help`: Print this help message.  \n"
+    "`!MFAImageBot <album-link> 2, 3, 42`: I attempt to link the 2nd, 3rd, and 42nd images from <album-link>  \n"
+    "`!MFAImageBot 2, 3, 42`: I attempt to link the 2nd, 3rd, and 42nd images from the album in the submission  \n"
+    "Comma or whitespace separated lists of numbers are also cool: `!MFAImageBot 2,4,42` or `!MFAImageBot 2 4 42`  \n"
+)
+TAIL = (
+    "\n\n---\nI am a bot! If you've found a bug you can open an issue "
+    "[here.](https://github.com/AlexBurkey/MFAImageBot/issues/new?template=bug_report.md)  \n"
+    "If you have an idea for a feature, you can submit the idea "
+    "[here](https://github.com/AlexBurkey/MFAImageBot/issues/new?template=feature_request.md)  \n"
+    "Version 1.2"
+)
