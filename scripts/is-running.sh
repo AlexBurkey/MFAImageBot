46867058b6e619805e1933f168dfb74b1626e916
Alex Burkey <alex.w.burkey@gmail.com>
2021-05-12 03:46:11 +0100

    Cleaned up run scripts

--- scripts/is-running.sh
+++ scripts/is-running.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-set -x #echo on
-
-# print env
-echo "~~~~~~"
-pwd
-date
-
+#set -x #echo on
 
 # TODO: Might also want to check if there are any python3 
 # processes running.
 # We could get multiple instances of bot processes running
-if [[ $(pgrep run.sh) ]]; then
-    echo "Bot is running"
-else
+if [[ ! $(pgrep run-mfaimage.sh) ]]; then
+# print env
+    echo "~~~~~~"
+    pwd
+    date
     echo "Bot is NOT running"
     echo "Restarting bot..."
     # Sends output of run.sh to ./logs/monitor
     # info about when and how many times the bot has crashed
-    nohup ./run.sh >> "./logs/monitor/$(date +"%FT%H%M%S").out" &
+    nohup ./run-mfaimage.sh >> "../logs/monitor/$(date +"%FT%H%M%S").out" &
 fi
